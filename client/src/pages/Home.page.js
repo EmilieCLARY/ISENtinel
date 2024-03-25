687687ade956085456305988305a93109c3b52b4
Alexis Mallet <alexis.mallet@student.junia.com>
2024-03-25 14:07:16 +0100

    socket setup

--- client/src/pages/Home.page.js
+++ client/src/pages/Home.page.js
@@ -1,9 +1,14 @@
 import { Button } from '@mui/material'
-import { useContext } from 'react';
+import { useContext, useEffect, useState  } from 'react';
 import { UserContext } from '../contexts/user.context';
- 
+
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:5000');
+
 export default function Home() {
  const { logOutUser } = useContext(UserContext);
+ const [response, setResponse] = useState('');
  
  // This function is called when the user clicks the "Logout" button.
  const logOut = async () => {
@@ -19,11 +24,23 @@ export default function Home() {
      alert(error)
    }
  }
+
+ const handleClick = () => {
+    // Emit a message to the server when the user clicks the button.
+    socket.emit('message', 'Hello from the client!');
+  }
+
+  // Listen for the server's response
+  socket.on('message received', (msg) => {
+    setResponse(msg);
+  });
  
  return (
    <>
      <h1>Welcome to ISENtinel</h1>
      <Button variant="contained" onClick={logOut}>Logout</Button>
+     <Button variant="contained" onClick={handleClick}>Send Message</Button>
+     <p>Server Response: {response}</p>
    </>
  )
 }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const PORT = 5000;
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+});
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.get('/api', (req, res) => {
     res.json({"users": ["user1", "user2", "user3"]})
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+
+    socket.on('message', (msg) => {
+        console.log('Button clicked :', msg);
+        // Send a response back to the client
+        socket.emit('message received', 'Button clicked! Server received your message.');
+      });
+});
+
+server.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
 });
